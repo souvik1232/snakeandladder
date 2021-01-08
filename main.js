047e3d5269de246a7f40af4e4fd0a2dc79b3837b
souvik1232 <souvik199831@gmail.com>
2021-01-08 09:39:27 +0530

    Added:Function to get dice roll

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 console.log("Welcome to Snake and Ladder");
 const snakeLadder = require('./utility');
 snakeLadder.assignPosition();
+snakeLadder.diceRoll();
--- utility.js
+++ utility.js
@@ -1,6 +1,10 @@
 class SnakeLadder {
     assignPosition=()=>{
         let playerOnePosition = 0;
+    }
+    diceRoll= ()=>{
+        let diceRoll = (Math.floor(Math.random() * 6)+1);
+        return diceRoll;
     }  
 }
 module.exports = new SnakeLadder();
